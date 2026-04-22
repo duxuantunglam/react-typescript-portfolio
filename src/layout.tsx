662e39f6b9876796c985969714591a3622bd06a6
duxuantunglam <duxuantunglam2k2@gmail.com>
2026-04-22 13:07:09 +0700

    hello world

---
+++ src/layout.tsx
@@ -0,0 +1,13 @@
+import TodoList from "./components/todo.list"
+
+function Layout() {
+
+  return (
+    <div>
+      <div>Hello World</div>
+      <TodoList />
+    </div>
+  )
+}
+
+export default Layout
